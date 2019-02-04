68dd98d546fe2a6e60e83b4ae6dc710dab935690
VALERY KHVARYHIN <valera.khvoryhin@gmail.com>
2019-02-05 01:19:21 +0300

    Изменил структуру корневого каталога

---
+++ test_client/test_client.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+with open('data.json') as json_file:
+    json_data = json.load(json_file)
+
+url = "http://127.0.0.1:5000/predict"
+# url = "http://127.0.0.1:8080/predict"
+response = requests.post(url, json=json_data)
+
+# print(json_data)
+print(f'{response.status_code} {response.text}')
